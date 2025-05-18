a2968649c7b679bab7c43b6caf9c74d79708d0da
BarryTheBee571 <yuan.zhuang9@education.nsw.gov.au>
2025-05-19 09:48:44 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ account2 = BankAccount("987654321", "Chris", 1000)
 print(f"Account 1 - Owner: {account1.get_owner_name()}, Balance: {account1.get_balance()}")
 print(f"Account 2 - Owner: {account2.get_owner_name()}, Balance: {account2.get_balance()}")
 
-account1.set_owner_name("Stephen Wirinata")
+account1.set_owner_name("Stephen Wirianata")
 account2.set_owner_name("Chris Wong")
 print(f"Updated Account 1 Owner: {account1.get_owner_name()}")
 account1.deposit(200)
